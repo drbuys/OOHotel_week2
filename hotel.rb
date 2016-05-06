39321391f0c817b2c45a2141830af220309fe9d5
Zak Buys <zakbuys85@gmail.com>
2016-05-06 18:23:32 +0100

    hotel has rooms, can check in guests based on room type and availability, and check out guests

--- hotel.rb
+++ hotel.rb
@@ -8,34 +8,6 @@ class Hotel
     @guests = []
   end
 
-  def check_availability(room)
-    return room if room.has_guest.length == 0
-  end
-
-  def find_all_suitable(guest)
-    return @rooms.find_all { |i| set_room_types_to_numbers(i) == guest.number_in_group }
-  end
-
-  def find_suitable(guest)
-    return @rooms.find { |i| (set_room_types_to_numbers(i) == guest.number_in_group) && (i.has_guest.length == 0) }
-    # return @rooms.find_index { |i| set_room_types_to_numbers(i) == guest.number_in_group }
-  end
-
-
-
-  def check_in(guest)
-    room = find_suitable(guest)
-
-    # avail = check_availability(@rooms[room])
-
-    if room != nil # && avail != nil
-      @rooms[room.number - 1].add_guest(guest)
-      return @guests << guest
-    else
-      return "Sorry, we don't have a suitable room for you"
-    end
-  end
-
   def set_room_types_to_numbers(room)
     type = room.type
     if type == "single"
@@ -47,6 +19,38 @@ class Hotel
     else
       return "No room available"
     end
+  end
+
+  # def check_availability(room)
+  #   return room if room.has_guest.length == 0
+  # end
+  #
+  # def find_all_suitable(guest)
+  #   return @rooms.find_all { |i| set_room_types_to_numbers(i) == guest.number_in_group }
+  # end
+
+  def find_suitable(guest)
+    return @rooms.find { |i| (set_room_types_to_numbers(i) == guest.number_in_group) && (i.has_guest.length == 0) }
+    # return @rooms.find_index { |i| set_room_types_to_numbers(i) == guest.number_in_group }
+  end
+
+  def check_in(guest)
+    room = find_suitable(guest)
+    # avail = check_availability(@rooms[room])
+    if room != nil # && avail != nil
+      @rooms[room.number - 1].add_guest(guest)
+      return @guests << guest
+    else
+      return "Sorry, we don't have a suitable room for you"
+    end
+  end
+
+  def check_out(guest)
+    room = @rooms.find { |i| i.has_guest == [guest] }
+    @rooms[room.number - 1].remove_guest(guest)
+    index = @guests.find_index { |i| i == guest }
+    @guests.delete_at(index)
+    return @guests
   end
 
 end
--- specs/hotel_spec.rb
+++ specs/hotel_spec.rb
@@ -12,14 +12,14 @@ class TestHotel < MiniTest::Test
     @guest3 = People.new("Princess Leia", 1, 1)
     @guest4 = People.new("Bobba Fett", 10, 1)
     @guest5 = People.new("Mary Poppins", 2, 3)
-    # @guests = [@guest1, @guest2, @guest3, @guest4, @guest5]
-
+    #@guests = [@guest1, @guest2, @guest3, @guest4, @guest5]
+    #@guests = [@guest5]
     @room1 = Room.new(1, "double", 120, [])
     @room2 = Room.new(2, "single", 80, [])
     @room3 = Room.new(3, "double", 100, [])
     @room4 = Room.new(4, "double", 120, [])
     @room5 = Room.new(5, "triple", 150, [])
-    @room6 = Room.new(6, "single", 80, [@guest5])
+    @room6 = Room.new(6, "single", 80, [])
     @rooms = [@room1, @room2, @room3, @room4, @room5, @room6]
 
     @sheldor = Hotel.new("Sheldor", @rooms)
@@ -37,23 +37,19 @@ class TestHotel < MiniTest::Test
     assert_equal([], @sheldor.guests)
   end
 
-  def test_checkin_guest_to_hotel
-    @sheldor.check_in(@guest3)
-    assert_equal("Princess Leia", @sheldor.guests[0].name)
-    assert_equal("Princess Leia", @sheldor.rooms[1].has_guest[0].name)
-  end
+
 
   def test_set_room_types_to_numbers
     assert_equal(3, @sheldor.set_room_types_to_numbers(@room5))
   end
 
-  def test_check_availability_is_nil
-    assert_equal(nil, @sheldor.check_availability(@room6))
-  end
-
-  def test_check_availability_is_available
-    assert_equal(@room5, @sheldor.check_availability(@room5))
-  end
+  # def test_check_availability_is_nil
+  #   assert_equal(nil, @sheldor.check_availability(@room6))
+  # end
+  #
+  # def test_check_availability_is_available
+  #   assert_equal(@room5, @sheldor.check_availability(@room5))
+  # end
 
   def test_suitability_of_rooms_for_guest3
     assert_equal(@room2, @sheldor.find_suitable(@guest3))
@@ -63,6 +59,23 @@ class TestHotel < MiniTest::Test
     @room5.has_guest = [@guest3]
     assert_equal( "Sorry, we don't have a suitable room for you" , @sheldor.check_in(@guest5))
   end
+
+  def test_checkin_guest_to_hotel
+    @sheldor.check_in(@guest3)
+    assert_equal("Princess Leia", @sheldor.guests[0].name)
+    assert_equal("Princess Leia", @sheldor.rooms[1].has_guest[0].name)
+  end
+
+  def test_checkout_guest5_from_room6
+    @sheldor.check_in(@guest2)
+    @sheldor.check_in(@guest3)
+    @sheldor.check_in(@guest4)
+    @sheldor.check_in(@guest5)
+    assert_equal( [@guest2, @guest3, @guest5], @sheldor.check_out(@guest4))
+    assert_equal([], @room6.has_guest)
+  end
+
+
 
   # def test_find_all_suitable_rooms_for_guest3
   #   assert_equal([@room2, @room6] , @sheldor.find_all_suitable(@guest3))
